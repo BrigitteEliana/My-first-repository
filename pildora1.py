cff820007228bef0361c39ac4ccffa40c337abfa
BrigitteEliana <99161630+BrigitteEliana@users.noreply.github.com>
2022-02-07 00:43:17 -0500

    Add files via upload
    Adding a file via upload

---
+++ pildora1.py
@@ -0,0 +1,10 @@
+print("hola mundo")
+print("hola gente"); print("adiosito") #se pueden hacer varias instrucciones en una sola líneacon el punto y coma, pero no es muy recomendado
+mi_nombre="mi nombre es Brigitte"
+mi_nombre="mi nombre es\
+Brigitte"
+print(mi_nombre) #la barra inclinada es para separar renglones en una misma línea de código
+a=0
+for i in range(5):
+    a+=1
+    print(a) 
